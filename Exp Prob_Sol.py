9eb73ec2c60c267c5758be7841c6a8b0696bddb4
itsshannu44 <shanmukhakabbinagadde@gmail.com>
2025-10-27 15:42:47 +0530

    commit

--- Exp Prob_Sol.py
+++ Exp Prob_Sol.py
@@ -1,14 +1,30 @@
-#______________Craete a list of all uopercase words form a given list.___________
+# Create a list of all uppercase words from a given list.
 
-words=["shannu",'vaibhav','shridhar']
-up_list=[word.upper() for word in words ]
+words = ["shannu", 'vaibhav', 'shridhar']
+up_list = [word.upper() for word in words]
 print(up_list)
 
+# Given a list of words, create a list with words that start with a vowel.
 
-#_______________Given a list of words, create a list with words that start with a vowel.__________
-
-words = ["shannu", "vaibhav", "shridhar"]
+words = ["shannu", "eat", "cat", 'owl', 'new']
 vowels = ('a', 'e', 'i', 'o', 'u')
 vowel_start_words = [word for word in words if word[0].lower() in vowels]
 print(vowel_start_words)
+
+# Given a list of strings, create a new list that contains the string and its length.
+strings = ['apple', 'banana', 'cherry', 'date']
+string_lengths = [(s, len(s)) for s in strings]
+print(string_lengths)
+
+#Extract all lowercase letters from a string into a set.
+input_string = "Hello World! This is a Test String 123."
+lowercase_letters = {char for char in input_string if char.islower()}   
+print(lowercase_letters)
+
+#Given a sentence , create a set of all words that have more than 4 chars.
+sentence = "This is an example sentence for creating a set of words"
+words = sentence.split()
+long_words = {word for word in words if len(word) > 4}
+print(long_words)
+
 
