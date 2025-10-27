3305e746c96cbbf6de1f93cb6af1c3aa928f36ef
itsshannu44 <shanmukhakabbinagadde@gmail.com>
2025-10-27 14:54:00 +0530

    commit

--- List Comp.py
+++ List Comp.py
@@ -36,3 +36,29 @@ print(l)
 
 l=[a*b for a in range(10) for b in range(5) if (a+b)%2==0]
 print(l)
+
+
+
+#======================SET=================
+
+#______________Simple Comprehension___________________
+
+s={a for a in [1,2,3,4,5]}
+print(s)
+
+#____________Conditional Coprehension________________
+
+s={a+2 for a in range(10) if a%2!=0}
+print(s)
+
+
+#____________Nested Comprehension__________________
+
+s={(a,b) for a in [1,2,3,4,5] for b in ['a','b','c']}
+print(s)
+
+
+#____________Nested Conditional Comprehension___________
+s={(a,b) for a in [1,2,3,4] for b in [4,5] if(a+b)%2==0}
+print(s)
+
