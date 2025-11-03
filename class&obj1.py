d1da165875a9ebd52ca0e6c9d7580dc78a6baa0b
itsshannu44 <shanmukhakabbinagadde@gmail.com>
2025-11-03 11:01:36 +0530

    commit

--- class&obj1.py
+++ class&obj1.py
@@ -1,3 +1,4 @@
+#----A dog has a name  and is of a particular breed and has an age. The dog barks and sleeps.
 class Dog:
     def __init__(self,name,breed, age):
         self.name=name
@@ -15,3 +16,25 @@ d2=Dog('Shiba','German Shepherd',8)
 
 d1.barks()
 d1.sleeps()
+
+#----A car has a make model & year the car can be started and can be stopped. Every car has an initial reading of 0.
+class Car:
+    def __init__(self, model, year):
+        self.model=model
+        self.year=year
+        self.reading=0
+
+    def start(self):
+        print(f'{self.model} of the year {self.year} is started')    
+
+    def stop(self):
+        print(f'{self.model} of the year {self.year} is stopped')
+    
+    def check_reading(self):
+        print(f"The {self.model} of the year {self.year} has run {self.reading}km.")
+
+c1=Car('urus','2022')
+c2=Car('Dezire','2020')
+c1.start()
+c1.check_reading()
+c2.stop()
