2ef9413cbf628715e1e5fdd80007054e6167f52f
itsshannu44 <shanmukhakabbinagadde@gmail.com>
2026-01-05 13:15:37 +0530

    commit

--- Python U4/req.py
+++ Python U4/req.py
@@ -1,5 +1,6 @@
 import requests as r
 import json
+import csv
 
 response=r.get('https://dummyjson.com/posts/120')
 print(type(response))
@@ -28,7 +29,38 @@ print(data)
 print(type(data))
 
 #Save the data to the json file with format
-
 with open('api_data.json','w') as f:
     json.dump(data, f, indent=4)
 print('Data Saved to file.')
+print("\n\n")
+
+
+#Write multiple api data into a json file
+response=r.get('https://dummyjson.com/posts')
+#convert the data to JSON Obj
+data=response.json()
+print(data)
+
+#save data to json
+#process data to individual rec
+posts_list=[]
+for post in data['posts']:
+    posts_list.append({'id':post['id'],
+                       'titlt':post['title'],
+                       'body':post['body'],
+                       'userId':post['userId'],
+                       'reactions':post['reactions']})
+print()
+print(posts_list)
+
+#store
+with open('api_data_1.json','w')as f:
+    json.dump(posts_list,f ,indent=4)
+print('Data inserted successfully')
+
+#save single api data into csv file
+with open('api_data.csv','w',newline="")as f:
+    writer=csv.writer(f)
+    writer.writerow(['Id','Title','Body','userId'])
+    writer.writerow([data['id'],data['title'],data['body'],data['userId']])
+print('Data stored in a csv file.')
