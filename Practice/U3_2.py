48a8a19e11ebf19687db7d811827207fdf399306
itsshannu44 <shanmukhakabbinagadde@gmail.com>
2026-01-15 17:57:57 +0530

    commit

--- Practice/U3_2.py
+++ Practice/U3_2.py
@@ -1,1 +1,10 @@
 #A program converts user input into an integer. Handle invalid inputs such as alphabetic  strings and sym
+
+def convert_to_int():
+    try:
+        n = int(input("Enter a number: "))
+        print("Converted integer is:", n)
+    except ValueError:
+        print("Invalid input! Please enter a valid integer.")
+
+convert_to_int()
