87d1055bde092aecfbca6f0ac44990951d2794f5
itsshannu44 <shanmukhakabbinagadde@gmail.com>
2026-01-15 17:53:35 +0530

    commit

---
+++ Practice/U3_1.py
@@ -0,0 +1,13 @@
+from math import sqrt
+
+def compute_sqrt():
+    try:
+        n=int(input("Enter the num to find sqrt: "))
+        if(n<0):
+            print("Value is negative")
+        else:
+            print("Square Root of number is:",sqrt(n))
+    except ValueError:
+        print(f'Value is Negative.')
+
+compute_sqrt()
