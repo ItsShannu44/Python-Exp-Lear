44827611dffc11efd9aac2589a404252937f6aad
itsshannu44 <shanmukhakabbinagadde@gmail.com>
2026-01-21 18:55:42 +0530

    init

--- Python U4/web_scraping/pr_static_scraping.py
+++ Python U4/web_scraping/pr_static_scraping.py
@@ -9,10 +9,23 @@ html=urlopen(url)
 print(type(url), url)
 
 soup=bs(html,'lxml')
-print(type(soup), soup)
+print(type(soup), soup ,"\n")
 
 title=soup.title
-print(type(title),title)
+print(type(title),title, "\n")
 
 text=soup.get_text()
-print(type(text),soup.text)
+print(type(text),soup.text ,"\n")
+
+
+a=soup.find_all('a','\n')
+print(a)
+for link in a:
+    print(link.get('href'))
+
+rows=soup.find_all('tr')
+print(rows)
+for row in rows:
+    row_td=soup.find_all('td')
+print(row_td, "\n")
+
