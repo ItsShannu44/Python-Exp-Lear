26041c2e2e8a3d1438d5bc33814fbd9ba9daacbf
itsshannu44 <shanmukhakabbinagadde@gmail.com>
2026-02-02 18:23:11 +0530

    commit

--- Python U1-U2/Recursion.py
+++ Python U1-U2/Recursion.py
@@ -1,11 +1,10 @@
-def factorial(n):
+def fact(n):
     if n==0 or n==1:
         return 1
-    else:
-        return n*factorial(n-1)
+    return n*fact(n-1)
 
 n=int(input("Enter the number"))
-print(f"The factorial of {n} is:", factorial(n))
+print(f"The factorial of {n} is:", fact(n))
 
 
 
